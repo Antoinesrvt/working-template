4c7ff255d2fae3a816bbb3518bdec722c5eb4bda
Antoine <68781066+Antoinesrvt@users.noreply.github.com>
2024-06-26 03:03:26 +0200

    Add or update src/components/ui/button.tsx

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,5 @@
+import { Button as ArkButton } from '@ark-ui/react'
+import { styled } from '@/styled-system/jsx'
+import { button } from '@/styled-system/recipes'
+
+export const Button = styled(ArkButton, button)
